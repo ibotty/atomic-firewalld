781ed5e8038e81e372c64eb5f076d469b9ca21de
Tobias Florek <tob@butter.sh>
2015-07-02 14:03:31 +0200

    copy firewalld config to host

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 if [ ! -d ${HOST}/etc/firewalld ] ; then
   mkdir ${HOST}/etc/firewalld
+  cp -a /etc/firewalld ${HOST}/etc/firewalld
   chroot ${HOST} restorecon -R /etc/firewalld
 fi
 
@@ -12,7 +13,7 @@ After=docker.service
 
 [Service]
 ExecStart=/usr/bin/docker run -d --rm --privileged --net=host -v /lib/modules:/lib/modules:ro -v /etc/firewalld: /etc/firewalld --name ${NAME} --entrypoint /sbin/firewalld --no-fork
-ExecReload=/usr/bin/docker exec -t ${NAME} /bin/sh -c '/bin/kill -HUP $(cat /run/firewalld.pid)'
+ExecReload=/usr/bin/docker exec -t ${NAME} /bin/sh -c '/bin/kill -HUP \$(cat /run/firewalld.pid)'
 
 [Install]
 WantedBy=multi-user.target
