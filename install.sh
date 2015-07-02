032dda47682d0e484508a302e3e205b9bc2cd518
Tobias Florek <tob@butter.sh>
2015-07-02 14:47:31 +0200

    copy dbus config for firewalld to host

--- install.sh
+++ install.sh
@@ -6,13 +6,15 @@ if [ ! -d ${HOST}/etc/firewalld ] ; then
   chroot ${HOST} restorecon -R /etc/firewalld
 fi
 
+cp -f /etc/dbus-1/system.d/FirewallD.conf ${HOST}/etc/dbus-1/system.d/FirewallD.conf
+
 cat <<EOF > ${HOST}/etc/systemd/system/firewalld.service
 [Unit]
 Description=FirewallD running in ${NAME}
 After=docker.service
 
 [Service]
-ExecStart=/usr/bin/docker run -d --rm --privileged --net=host -v /lib/modules:/lib/modules:ro -v /etc/firewalld:/etc/firewalld --name ${NAME} --entrypoint /sbin/firewalld --no-fork
+ExecStart=/usr/bin/docker run --rm --privileged -v /var/run/dbus:/var/run/dbus --net=host -v /lib/modules:/lib/modules:ro -v /etc/firewalld:/etc/firewalld --name ${NAME} --entrypoint /sbin/firewalld ibotty/atomic-firewalld --no-fork
 ExecReload=/usr/bin/docker exec -t ${NAME} /bin/sh -c '/bin/kill -HUP \$(cat /run/firewalld.pid)'
 
 [Install]
